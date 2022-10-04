971e18678c9fc3514bd47a1a517426996a42cb1f
donyfajarr <donyfajarr@gmail.com>
2022-10-05 01:52:11 +0700

    coba add jenislayanan

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ def home(request):
 
 def perbarui(request, id):
     # pemesananobj = models.pemesanan.objects.get(idpemesanan=id)
-    pemesananobj = models.pemesanan.objects.get(idpemesanan=id)
+    pemesananobj = models.pemesanan.objects.get(idpaketcpelanggan=id)
     if request.method == "GET":
         return render(request, 'perbarui.html', {
             "allpemesananobj" : pemesananobj
@@ -36,15 +36,19 @@ def index(request):
 
 def createdata (request):
     paket = models.paketlayanan.objects.all()
+    layanan = models.layanan.objects.all()
     if request.method == "GET":
         return render (request, 'createdata.html',{
-            'pakettersedia' : paket
+            'pakettersedia' : paket,
+            'layanantersedia' : layanan
 
         })
     else:
         # idpemesanan = request.POST['idpemesanan']
         idpaketpelanggan = request.POST['idpaketpelanggan']
         getpaketbaru = models.paketlayanan.objects.get(idpaketlayanan = idpaketpelanggan) #harus di get dulu baru bisa. karena dari html pas ngepost itu hasilnya cuma string. misal kalian di html pilih paket 1. itu datanya yang kekirim cuma "1". jadi harus diget dulu, jadiin si "1" itu sebagai parameter.
+        idlayanan = request.POST['idlayanan']
+        getlayanan = models.layanan.objects.get(idlayanan = idlayanan)
         nama = request.POST['nama']
         platnomor = request.POST ['platnomor']
         tanggalpesan = request.POST ['tanggalpesan']
@@ -53,6 +57,7 @@ def createdata (request):
         newpemesanan = models.pemesanan(
             # idpemesanan = idpemesanan,
             idpaketpelanggan = getpaketbaru, #harusnya idpaketpelanggan, bukan idpaketlayanan. soalnya di models itu id paket pelanggan. Kalau namain variabel gausa ribet biar ga salah
+            idlayanan = getlayanan,
             nama = nama,
             platnomor = platnomor,
             tanggalpesan = tanggalpesan
